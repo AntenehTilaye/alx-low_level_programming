6225303f7d39bb3103cffe4a2a16488456360b9f
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-10 18:32:52 +0300

    printf done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio>
+
+/**
+ * main- the starting point
+ * Return: it returns 0 for os
+ * Description: the program prints a string to STD output
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
