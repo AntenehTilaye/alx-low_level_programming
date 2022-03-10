7cd486c3eaddeae7eb8d60c39ba4f67cdfd16571
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-10 18:17:38 +0300

    the first c programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main- program starting point
+ * Return: it return 0 to the os
+ * Description: it is the starting point to my program
+ **/
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
