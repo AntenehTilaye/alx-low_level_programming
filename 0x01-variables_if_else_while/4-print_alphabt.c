915b541311646ec11e8af3d71f801d27622660cf
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-11 18:19:14 +0300

    print letter except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main- starting point
+ * Return: returns 0 for os
+ * Description: this program prints some text based on if for and while loops
+ **/
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
