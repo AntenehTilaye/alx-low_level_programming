39e2dc19d4f053613be7508f70d7c7b216015ed3
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-11 18:00:17 +0300

    print all lowercase character

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main- starting point
+ * Return: returns 0 for os
+ * Description: this program checks if the number is positive or not
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i < 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
