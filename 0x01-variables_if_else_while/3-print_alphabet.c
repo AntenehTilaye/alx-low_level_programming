e3c30c40834b3ce09a9cbfcd77e785f1c13bf156
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-11 18:10:54 +0300

    print lower and upper case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main- starting point
+ * Return: returns 0 for os
+ * Description: this program checks if the number is positive or not
+ */
+
+int main(void)
+{
+	int i = 'a';
+	int j = 'A';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j <= 'Z')
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+}
