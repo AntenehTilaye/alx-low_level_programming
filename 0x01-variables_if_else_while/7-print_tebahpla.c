4e60199482d9a958c70375027a44159281de8d50
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-11 18:48:02 +0300

    print reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main- starting point
+ * Return: returns 0 for os
+ * Description: this program checks if the number is positive or not
+ */
+
+int main(void)
+{
+	int i = 'z';
+	
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
