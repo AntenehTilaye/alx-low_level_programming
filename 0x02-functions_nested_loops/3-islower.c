61c9773c3ee77dc43c23668aa0830cac43db35d2
AntenehTilaye <antenehtilaye19@gmail.com>
2022-03-16 01:30:16 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - check lower caseletters
+ * @c: char to check
+ *
+ * Return: 0 or 1 will be returned
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
