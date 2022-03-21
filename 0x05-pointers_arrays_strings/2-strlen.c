016f63f6f0f1c44968c92e7f65fa9cc7e9cc7338
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-03-21 18:00:24 +0300

    str len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - check for upper case letter
+ * @n : first param
+ * Return : returns int
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while(str[i++] != '\0')
+	{	
+	}
+	return i;
+}
