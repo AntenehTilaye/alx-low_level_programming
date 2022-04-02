66de87fc7b73ebd64dcfb83b9701ef8e2d11338a
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-04-02 14:22:30 +0300

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int argc;
+	
+	printf("%s\n", argv[0]);
+	return (0);
+}
